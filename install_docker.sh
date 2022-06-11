347c14e352da8d85ee83abf8ba224aaf70f1f462
Hai Au <pnhaiau@gmail.com>
2022-06-11 22:04:06 +0700

    Run docker withou sudo in the end

--- install_docker.sh
+++ install_docker.sh
@@ -16,14 +16,14 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-#RUN DOCKER WITHOUT SUDO
-sudo groupadd docker
-sudo usermod -aG docker ${USER}
-newgrp docker 
-
 #INSTALL DOCKER COMPOSE
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
+
+#RUN DOCKER WITHOUT SUDO
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+newgrp docker 
